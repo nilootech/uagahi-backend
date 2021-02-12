eb5f2153d792b1c47935635299dd3999f9b1c4f8
Morteza <info@nilootech.com>
2021-02-12 13:43:55 +0330

    refactor directory of resolvers

--- src/models/user/user.module.ts
+++ src/models/user/user.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './user.schema';
 import { UserService } from './user.service';
-import { UserResolver } from '../../resolvers/user.resolver';
+import { UserResolver } from './user.resolver';
 
 @Module({
   imports: [
--- src/models/user/user.resolver.ts
+++ src/models/user/user.resolver.ts
@@ -1,11 +1,11 @@
-import { UserService } from '../models/user/user.service';
+import { UserService } from './user.service';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { UserType } from '../models/user/user.type';
-import { CreateUserInput } from '../models/user/inputs/create-user.input';
-import { Roles } from '../auth/role/role.decorator';
-import { Role } from '../auth/role/role.enum';
+import { UserType } from './user.type';
+import { CreateUserInput } from './inputs/create-user.input';
+import { Roles } from '../../auth/role/role.decorator';
+import { Role } from '../../auth/role/role.enum';
 import { UseGuards } from '@nestjs/common';
-import { GqlAuthGuard } from '../auth/guard/auth-guard.service';
+import { GqlAuthGuard } from '../../auth/guard/auth-guard.service';
 
 @Resolver(() => UserType)
 export class UserResolver {
@@ -13,7 +13,7 @@ export class UserResolver {
 
   @Query(() => [UserType])
   @UseGuards(GqlAuthGuard)
-  @Roles(Role.User)
+  @Roles(Role.Admin)
   users() {
     return this.userService.findAll();
   }
